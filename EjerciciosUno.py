e964c8053237fdce7a53c67664bcaeac1cd6d811
Salacho408 <newssalazargil@gmail.com>
2024-12-03 19:09:00 -0500

    Ejercicio 1.6

--- EjerciciosUno.py
+++ EjerciciosUno.py
@@ -61,7 +61,7 @@ def sumarMayorMenor(a, b, c, d, e):
     ans = (numMayor + numMenor)
     return ans
 
-EjercicioTres = sumarMayorMenor(8, 7, 3, 4, 2)
+EjercicioTres = sumarMayorMenor(6, 1, 7, 2, 3)
 print("Ejercicio tres \n", EjercicioTres, "\n")
 
 # Ejercicio 4
@@ -101,5 +101,34 @@ def cuantosEnCuadrante(x1, y1, x2, y2, x3, y3, x4, y4, c):
     ans += sumarEnCuadrante(cuartoPunto, c)
     return ans
 
-EjercicioCinco = cuantosEnCuadrante(8, 7, 3, 4, 2, 3, 8, 7, 3)
+EjercicioCinco = cuantosEnCuadrante(2, 3.4, 3, 1, -2.1, -3, -4, 1, 1)
 print("Ejercicio cinco \n", EjercicioCinco, "\n")
+
+# Ejercicio 6
+
+def compararCartas(a, b, c, d, e):
+    cantidad = 1
+    if a == b:
+        cantidad += 1
+    if a == c:
+        cantidad += 1
+    if a == d:
+        cantidad += 1
+    if a == e: 
+        cantidad += 1
+    return cantidad
+
+def tienePoquer(cartaUno, cartaDos, cartaTres, cartaCuatro, cartaCinco):
+    c1 = compararCartas(cartaUno, cartaDos, cartaTres, cartaCuatro, cartaCinco)
+    c2 = compararCartas(cartaDos, cartaUno, cartaTres, cartaCuatro, cartaCinco)
+    c3 = compararCartas(cartaTres, cartaUno, cartaDos, cartaCuatro, cartaCinco)
+    c4 = compararCartas(cartaCuatro, cartaUno, cartaDos, cartaTres, cartaCinco)
+    c5 = compararCartas(cartaCinco, cartaUno, cartaDos, cartaTres, cartaCuatro)
+    if c1 == 4 or c2 == 4 or c3 == 4 or c4 == 4 or c5 == 4:
+        ans = True
+    else:
+        ans = False
+    return ans
+
+EjercicioSeis = tienePoquer("A", "2", "2", "2", "Q")
+print("Ejercicio seis \n", EjercicioSeis, "\n")
